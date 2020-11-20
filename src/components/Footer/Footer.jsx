2a669addfd539f24f54009f233bff51afaf3d498
Noman Shoukat <nomanaadma@gmail.com>
2020-11-20 14:30:12 +0500

    Header Footer Correction

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import Tooltip from '@material-ui/core/Tooltip';
 
 function Footer() {
 	return (
@@ -12,6 +13,7 @@ function Footer() {
 			</p>
 			<ul>
 				<li>
+					<Tooltip title="nomanaadma@gmail.com">
 					<a
 						href="mailto:nomanaadma@gmail.com"
 						target="_blank"
@@ -19,8 +21,10 @@ function Footer() {
 					>
 						<FontAwesomeIcon className={styles.email+' fa-lg'} icon={faEnvelope} />
 					</a>
+					</Tooltip>
 				</li>
 				<li>
+					<Tooltip title="LinkedIn">
 					<a
 						href="https://www.linkedin.com/in/nomanaadma/"
 						target="_blank"
@@ -28,8 +32,10 @@ function Footer() {
 					>
 						<FontAwesomeIcon className={styles.linkedin+' fa-lg'} icon={faLinkedinIn} />
 					</a>
+					</Tooltip>
 				</li>
 				<li>
+					<Tooltip title="Github">
 					<a
 						href="https://github.com/nomanaadma"
 						target="_blank"
@@ -37,8 +43,10 @@ function Footer() {
 					>
 						<FontAwesomeIcon className={styles.github+' fa-lg'} icon={faGithub} />
 					</a>
+					</Tooltip>
 				</li>
 				<li>
+					<Tooltip title="Facebook">
 					<a
 						href="https://www.facebook.com/NomanAadma"
 						target="_blank"
@@ -46,6 +54,7 @@ function Footer() {
 					>
 						<FontAwesomeIcon className={styles.facebook+' fa-lg'} icon={faFacebookF} />
 					</a>
+					</Tooltip>
 				</li>
 			</ul>
 		</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import classes from "./Header.module.css";
 import logo from '../../images/logo.png';
