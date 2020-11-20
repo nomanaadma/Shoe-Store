74d36e952967b841b6082c56fbdfbc94e95f2ab6
Noman Shoukat <nomanaadma@gmail.com>
2020-11-21 01:37:11 +0500

    Products

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { Header, Home, Product, ProductDetails, About, Contact, Footer, NotFound } from './components';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Switch>
-                <Route path="/" exact component={Home} />
-				<Redirect from="/home" exact to="/" />
-                <Route path="/product/:id" component={ProductDetails} />
-                <Route path="/products" component={Product} />
-				<Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-				<Route path="/not-found" component={NotFound} />
-				<Redirect to="not-found" />
-			</Switch>
+			<Container maxWidth="xl" className="wrapper">
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Redirect from="/home" exact to="/" />
+					<Route path="/product/:id" component={ProductDetails} />
+					<Route path="/products" component={Product} />
+					<Route path="/about" component={About} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/not-found" component={NotFound} />
+					<Redirect to="not-found" />
+				</Switch>
+			</Container>
 			<Footer />
 		</>
 	)
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,81 @@
-function Product() {
-	return ('Product');
+import {
+	Grid,
+	Card,
+	CardActionArea,
+	CardContent,
+	CardMedia,
+	Typography,
+} from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
+import shoesDetails from "../../data/ShoesDetails";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import classes from "./Product.module.css";
+import { Link } from "react-router-dom";
+
+function Products() {
+	const images = require.context("../../", true);
+
+	return (
+		<Grid container spacing={2}>
+			{shoesDetails.map((shoe, index) => {
+				return (
+					<Grid
+						item
+						xs={12}
+						sm={6}
+						md={4}
+						className={classes.gridParentForRating}
+						key={index}
+					>
+						<Rating
+							className={classes.rating}
+							name="customized-empty"
+							defaultValue={shoe.rating}
+							precision={0.1}
+							emptyIcon={<StarBorderIcon fontSize="inherit" />}
+						/>
+						<span className={classes.priceTag}>${shoe.price}</span>
+						<Link
+							to={`/product/${shoe.id}`}
+							className={classes.gridLink}
+						>
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt={shoe.name}
+										height="300"
+										image={
+											images(`.${shoe.images[0]}`).default
+										}
+										title={shoe.name}
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+											className={classes.homeCardName}
+										>
+											{shoe.name}
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+											className={classes.homecarddesc}
+										>
+											{shoe.desc}
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+							</Card>
+						</Link>
+					</Grid>
+				);
+			})}
+		</Grid>
+	);
 }
 
-export default Product;
+export default Products;
