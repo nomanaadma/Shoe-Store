f5d61bf8bd6318b7e7e03f90a02d73d4554c790f
Noman Shoukat <nomanaadma@gmail.com>
2020-11-21 19:32:02 +0500

    Contact and 404

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,27 @@
+import classes from './Contact.module.css';
+import { Card, CardContent, Typography, TextField, Button } from '@material-ui/core';
+
 function Contact() {
-	return ('Contact');
+	return (
+		<Card className={classes.root}>
+			<CardContent>
+				<Typography variant="h4" className={classes.heading} gutterBottom>Contact</Typography>
+				<TextField label="Name" className={classes.field} />
+				<TextField label="Email" className={classes.field} />
+				<TextField label="Contact" className={classes.field} />
+				<TextField
+					className={classes.field+' '+classes.textarea}
+					label="Message"
+					multiline
+					rows={4}
+					variant="outlined"
+				/>
+				<Button variant="contained" color="primary" className={classes.field+' '+classes.button}>
+					Contact
+				</Button>
+			</CardContent>
+		</Card>
+	);
 }
 
 export default Contact;
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Typography } from '@material-ui/core';
+
 function NotFound() {
-	return ('NotFound');
+	return (		
+		<div style={{ textAlign: 'center', height: 'calc(100vh - 317px)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+			<div>
+				<img src={require('../../images/404.png').default} alt="404" />
+				<Typography variant="h4" gutterBottom>404 - Page Not Found</Typography>
+				<p>The page you are looking for might have been removed <br /> had its name changed or is temporarily unavailable</p>
+			</div>
+		</div>
+	);
 }
 
 export default NotFound;
