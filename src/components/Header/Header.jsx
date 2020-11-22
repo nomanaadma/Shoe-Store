0c37aec5c12e1d5e77aea609620f6c165aa758af
Noman Shoukat <nomanaadma@gmail.com>
2020-11-22 15:43:25 +0500

    Router Issue of active and data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,23 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import classes from "./Header.module.css";
 import logo from '../../images/logo.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation  } from 'react-router-dom';
 
 function Header() {
+
+	const location = useLocation();
+	const activeClassName = 'active-nav';
+	const activeProductDetail = location.pathname.includes('product/') ? activeClassName : '';
 
 	return (
 		<div className={classes.root}>
 			<AppBar position="static" className={classes.navbar}>
 				<Toolbar>
 					<div className={classes.logo}><img src={logo} style={{ width: '175px' }} alt="Logo" /></div>
-					<NavLink to="/" exact activeClassName="active-nav" className={classes.menu}><Button color="inherit">Home</Button></NavLink>
-					<NavLink to="/products" activeClassName="active-nav" className={classes.menu}><Button color="inherit">Products</Button></NavLink>
-					<NavLink to="/about" activeClassName="active-nav" className={classes.menu}><Button color="inherit">About</Button></NavLink>
-					<NavLink to="/contact" activeClassName="active-nav" className={classes.menu}><Button color="inherit">Contact</Button></NavLink>
+					<NavLink to="/" exact activeClassName={activeClassName} className={classes.menu}><Button color="inherit">Home</Button></NavLink>
+					<NavLink to="/products" activeClassName={activeClassName} className={classes.menu+' '+activeProductDetail}><Button color="inherit">Products</Button></NavLink>
+					<NavLink to="/about" activeClassName={activeClassName} className={classes.menu}><Button color="inherit">About</Button></NavLink>
+					<NavLink to="/contact" activeClassName={activeClassName} className={classes.menu}><Button color="inherit">Contact</Button></NavLink>
 				</Toolbar>
 			</AppBar>
 		</div>
